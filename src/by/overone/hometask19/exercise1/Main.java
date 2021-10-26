4dfe40fe72f1a1a5d24f6b6d4cff6d19c425243c
Andrei Hlaz <hlander14@gmail.com>
2021-10-26 23:28:02 +0300

    Add one Hello

--- src/by/overone/hometask19/exercise1/Main.java
+++ src/by/overone/hometask19/exercise1/Main.java
@@ -3,5 +3,6 @@ package by.overone.hometask19.exercise1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GIT!!!!");
+        System.out.println("Hello GIT!!!!");
     }
 }
