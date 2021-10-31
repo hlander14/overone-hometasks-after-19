77597217d2a50b55c5855bc5c82bac1b1c90b8c9
Andrei Hlaz <hlander14@gmail.com>
2021-10-31 17:17:17 +0300

    Temp

---
+++ src/by/overone/hometask20/exercise1/MainStudent.java
@@ -0,0 +1,13 @@
+package by.overone.hometask20.exercise1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MainStudent {
+    public static void main(String[] args) {
+        ArrayList<Integer> evaluations = new ArrayList<>();
+        Iterator<Integer> iterator = evaluations.iterator();
+
+        
+    }
+}
